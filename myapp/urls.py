a479d6d715205537098d55d505756a83c51308fd
S1lencex <91425329+S1lencex@users.noreply.github.com>
2022-04-03 18:00:27 +0300

    Create urls.py

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add-author', views.AuthorCreateView.as_view(), name="add-author"),
+    path('add-book', views.new_book, name="add-book"),
+    path('book/<int:pk>/renew', views.renew_book_librarian, name='renew-book-librarian'),
+]
